29065d096aa525e9898dcbb404e6530359bda041
machinelatto <machinelatto@gmail.com>
2017-11-13 22:42:08 -0200

    algoritmo para checar os primos está mais rápido, passou em todos os testes

--- src/main.c
+++ src/main.c
@@ -6,12 +6,41 @@
  */
 
 #include <stdio.h>
+#include <sys/time.h>
+
+int ehprimo(int n){
+	int i;
+	if(n==1 || n==0) return 0;
+	if(n==2) return 1;
+	if(n%2==0) return 0; //pares
+	for(i=3;i<=n/2;i=i+2){  //testa os ímpares
+		if(n%i==0){ //caso o resto de alguma divisão seja 0, encontro um divisor, ou seja ele não é primo
+			return 0;
+		}
+	}
+	return 1;
+}
 
 int main() {
+	//struct timeval rt0, rt1, drt;
+	//gettimeofday(&rt0, NULL);
+	
+	int num=0;
+	FILE *fp; //crio o pipe
 
-  int x, y;
+	fp = popen("sort -n -r","w"); //abro o pipe, w indica write
 
-  scanf("%d %d\n", &x, &y);
-  printf("%d\n", x + 200);
-  return 0;
+	while(num!=-1){
+		scanf("%d",&num);
+		if(!ehprimo(num) && (num!=-1)){
+			fprintf(fp, "%d\n", num); //escrevo no pipe cada número nao primo, o pipe ja ordena eles
+		}
+	}
+
+	pclose(fp); //fecho
+
+	//gettimeofday(&rt1, NULL);
+    //timersub(&rt1, &rt0, &drt);  
+    //printf("Tempo: %ld.%06ld segundos\n", drt.tv_sec, drt.tv_usec);
 }
+
